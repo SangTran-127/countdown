a10bb9c276933cdd2ad817237cbbc9c31ab80409
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-05-19 19:34:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Timer from './components/Timer/Timer'
+import PacmanLoader from "react-spinners/PacmanLoader";
 function App() {
+  const loaderStyle = {
+    position: "absolute",
+    left: "40%",
+    top: "30%",
+    transform: "translate(-50%, -50%)"
+  }
+  const [loading, setLoading] = useState(false)
+  useEffect(()=> {
+    setLoading(true)
+    setTimeout(()=> {
+      setLoading(false)
+    }, 4000)
+  }, [])
   return (
     <>
-      <Timer />
+      {loading ? 
+      <div style={loaderStyle}>
+      <PacmanLoader 
+      color="#6bbe92" 
+      loading={loading}  
+      size={150}         
+      /> 
+      </div>
+      :<Timer />}
+      
     </>
   )
 }
